06848582773b1e7afe7f4e459e58878266f3e4ce
obarnett <olivia.barnett@disneystreaming.com>
2020-11-06 10:53:19 -0500

    Initial Commit

---
+++ pumpkin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pumpkin
+//
+//  Created by Barnett, Olivia on 11/6/20.
+//  Copyright © 2020 Barnett, Olivia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
